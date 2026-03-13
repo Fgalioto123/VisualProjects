cc86ad40421834e175fbe02a8fc1952802db9340
Fgalioto123 <galioto.fox.26@c-dh.org>
2026-03-13 13:40:40 -0500

    ttt

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -6,22 +6,27 @@ public class TicTacToe {
     int boardWidth = 600;
     int boardHeight = 650; //50px for text panel on top
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic-Tac-Toe");
     JLabel textLable = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton b1 = new JButton("Button 1");
 
     public static void main(String[] args) {
         
     }
 
     TicTacToe() {
-        frame.setSize(boardWidth, boardHeight);
+        //creating the frame
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
         
+        //creating the top label
         textLable.setBackground(Color.darkGray);
         textLable.setForeground(Color.white);
         textLable.setFont(new Font("Arial", Font.BOLD, 50));
@@ -32,5 +37,11 @@ public class TicTacToe {
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLable);
         frame.add(textPanel, BorderLayout.NORTH);
+
+        //creating the board
+        boardPanel.setLayout(new GridLayout(3, 3));
+        boardPanel.setBackground(Color.darkGray);
+        boardPanel.add(b1);
+        frame.add(boardPanel);
     }
 }
